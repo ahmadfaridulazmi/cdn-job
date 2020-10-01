d3bbc4d43ad0fd05051515805f9ce26bb54c9c95
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2020-10-01 09:42:16 +0800

    congifure db production

--- knexfile.js
+++ knexfile.js
@@ -39,18 +39,15 @@ const _config = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: __dirname + '/db/migrations'
     }
   }
 
