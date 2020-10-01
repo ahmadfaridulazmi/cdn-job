02d4025f4ae217358681972cada0975cb77bf68d
Faridul Azmi <ahmadfaridulazmi@gmail.com>
2020-10-01 11:32:17 +0800

    remove ssl for pg production

--- knexfile.js
+++ knexfile.js
@@ -40,7 +40,7 @@ const _config = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
